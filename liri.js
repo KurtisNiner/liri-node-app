859c1eb2f9ff858c1f3326843ff109821cd2e903
KurtisNiner <kcarpe28@gmail.com>
2018-03-04 21:15:38 -0500

    go baby go

--- liri.js
+++ liri.js
@@ -11,15 +11,12 @@ var doThis = process.argv[2];
 
 // console.log(process.argv[2]);
 
-// var command = process.argv[2];
-
 switch (doThis) {
     case "my-tweets": tweets(); break;
     case "spotify-this-song": spotifySong(); break;
     case "movie-this": movieThis(); break;
     case "do-what-it-says": doWhatItSays(); break;
-
-    default: console.log("ok");
+    default: console.log("command options: my-tweets, spotify-this-song, movie-this, do-what-it-says");
 };
 
 
@@ -30,13 +27,13 @@ function tweets() {
         //  console.log(response);
         if (!error) {
             for (var i = 0; i < tweets.length; i++) {
-                var myTweets = 
-                "\r\n"
+                var myTweets =
+                    "\r\n"
                     + "kurtis posted: "
                     + tweets[i].text
                     + "\n"
                 console.log(myTweets);
-               
+
             }
         }
     })
@@ -46,19 +43,19 @@ function tweets() {
 
 function spotifySong() {
     var songInput = process.argv[3];
-    if(!songInput){
+    if (!songInput) {
         songInput = "livin on a prayer";
         (console.log("\r\n" + "because you didn't put in a song, I give you the best song ever!" + "\n"))
     }
-    spotify.search({ type: "track", query: songInput, limit: 3}, function (err, data) {
+    spotify.search({ type: "track", query: songInput, limit: 3 }, function (err, data) {
         if (!err) {
             var songInformation = data.tracks.items;
             // console.log(songInformation);
             for (var i = 0; i < 3; i++) {
                 if (songInformation[i] != undefined) {
                     var songResult =
-                    "The Song Informaiton you requested is: " + "\n"
-                        +"\n"
+                        "The Song Informaiton you requested is: " + "\n"
+                        + "\n"
                         + "*The artist is: " + songInformation[i].artists[0].name + "\n"
                         + "*The song name is: " + songInformation[i].name + "\n"
                         + "*A link to the song: " + songInformation[i].preview_url + "\n"
@@ -70,46 +67,52 @@ function spotifySong() {
             }
         }
         else {
-            console.log("dude, i'm sorry, but an error has occurred " + err); 
+            console.log("dude, i'm sorry, but an error has occurred " + err);
         }
     });
 };
 
 //movie-this function, which takes a movie you want, and gives you info on it
 
-function movieThis(){
+function movieThis() {
     var movieInput = process.argv[3];
-    if(!movieInput){
+    if (!movieInput) {
         movieInput = "Mr Nobody";
         console.log(movieInput);
     }
-    request("http://www.omdbapi.com?apikey=trilogy&t=" + movieInput + "&y=&plot=short&tomatoes=true&r=json", function(error, response, body){
-        if(!error){
+    request("http://www.omdbapi.com?apikey=trilogy&t=" + movieInput + "&y=&plot=short&tomatoes=true&r=json", function (error, response, body) {
+        if (!error) {
             var movieOutput = JSON.parse(body);
             // console.log(body);
         }
-        var movieStuff = 
-        "Here is some information that you have requested about this movie"
-        + "\r\n" 
-        + "\n" 
-        + "Title: " + movieOutput.Title + "\n"
-        + "Released in " + movieOutput.Year + "\n"
-        + "IMDB rating: " + movieOutput.imdbRating + "\n"
-        + "Rotton Tomatoes Rating" + movieOutput.tomatoRating + "\n"
-        + "This movie was produced in " + movieOutput.Country + "\n"
-        + "This movies language is " + movieOutput.Language + "\n"
-        + "Plot: " + movieOutput.Plot + "\n"
-        + "Cast" + movieOutput.Actors + "\n"
-        + "__________________________________________________"
+        var movieStuff =
+            "Here is some information that you have requested about this movie"
+            + "\r\n"
+            + "\n"
+            + "Title: " + movieOutput.Title + "\n"
+            + "Released in " + movieOutput.Year + "\n"
+            + "IMDB rating: " + movieOutput.imdbRating + "\n"
+            + "Rotton Tomatoes Rating" + movieOutput.tomatoRating + "\n"
+            + "This movie was produced in " + movieOutput.Country + "\n"
+            + "This movies language is " + movieOutput.Language + "\n"
+            + "Plot: " + movieOutput.Plot + "\n"
+            + "Cast" + movieOutput.Actors + "\n"
+            + "__________________________________________________"
         console.log(movieStuff);
     })
-    
+
 }
-// * Title of the movie.
-// * Year the movie came out.
-// * IMDB Rating of the movie.
-// * Rotten Tomatoes Rating of the movie.
-// * Country where the movie was produced.
-// * Language of the movie.
-// * Plot of the movie.
-// * Actors in the movie.
+
+//doWhatItSays function takes "i want it that way" from random.txt and runs the spotifySong function
+function doWhatItSays() {
+
+    fs.readFile("random.txt", "utf8", function (error, data) {
+        if (error) {
+            return console.log("an error occurred" + error);
+        }
+        var doWhat = data.split(",");
+        console.log(doWhat[0]);
+        var songInput = doWhat;
+        spotifySong();
+    });
+}
